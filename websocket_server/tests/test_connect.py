963eccf7b6e570dde47b49f0a835f8d6869d81de
Quintin Dunn <93884113+quintindunn@users.noreply.github.com>
2023-12-09 16:47:39 -0500

    Cleaned up test_connection.py

--- websocket_server/tests/test_connect.py
+++ websocket_server/tests/test_connect.py
@@ -8,17 +8,26 @@ import websocket
 
 class TestConnection(TestCase):
     def test_connect(self):
-        msg = f"Test {str(uuid.uuid4())}"
+        """
+        Connects to the websocket, sends an echo message, checks they're equivalent.
+        :return:
+        """
 
+        # Connect to the websocket.
         ws = websocket.WebSocket(debug=True)
         ws.connect("ws://127.0.0.1:9090")
 
+        # Generate the message
+        msg = f"Test {str(uuid.uuid4())}"
+
+        # Generate the packet to send.
         send = json.dumps({
             "type": "echo",
             "content": msg
         })
 
         ws.send(send)
-        recv = ws.recv()
 
+        # Check the response is equal to the data sent.
+        recv = ws.recv()
         self.assertEqual(recv, send)
